725f5550d0e1b1bc2acd217b3b53e2555f01bcac
hammsik <asbbrp@gmail.com>
2023-05-04 16:17:32 +0900

    변수 연습 - var keyword

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class SikGradientContainer extends StatelessWidget {
   const SikGradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 0, 0, 0),
             Color.fromARGB(255, 57, 255, 156),
           ],
-          begin: Alignment.topLeft,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: SikStyledText("메이플스토리끼에엑"),
+        child: SikStyledText("HelloMyyyWorld!"),
       ),
     );
   }
